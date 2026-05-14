e84b5a034f52e21ef23504854625c7f57483de15
SnehaAdki <adkisneha1201@gmail.com>
2026-05-14 19:33:56 +0530

    Add files via upload

---
+++ Snowflake_Queries/Triangle_Judgement.sql
@@ -0,0 +1,54 @@
+-- 610. Triangle Judgement
+
+-- Table: Triangle
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | x           | int  |
+-- | y           | int  |
+-- | z           | int  |
+-- +-------------+------+
+-- In SQL, (x, y, z) is the primary key column for this table.
+-- Each row of this table contains the lengths of three line segments.
+ 
+
+-- Report for every three line segments whether they can form a triangle.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+-- Example 1:
+
+-- Input: 
+-- Triangle table:
+-- +----+----+----+
+-- | x  | y  | z  |
+-- +----+----+----+
+-- | 13 | 15 | 30 |
+-- | 10 | 20 | 15 |
+-- +----+----+----+
+-- Output: 
+-- +----+----+----+----------+
+-- | x  | y  | z  | triangle |
+-- +----+----+----+----------+
+-- | 13 | 15 | 30 | No       |
+-- | 10 | 20 | 15 | Yes      |
+-- +----+----+----+----------+
+
+
+
+with Traingle (x,y,z) as (
+select * from values 
+(13,15,30),
+(10,20,15) t(x,y,z)
+)
+select *,
+case 
+    when x+y > z and y+z > x and z+x > y 
+    then 'Yes ' 
+    else 'No'
+end as Triangle
+from Traingle
+;
